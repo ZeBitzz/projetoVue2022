b720250edbe3119a0758b1315272465cd1b90024
ZeBitzz <9180270@esmad.ipp.pt>
2022-01-17 14:43:21 +0000

    design login e register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,11 +2,16 @@
   <div>
     <br />
     <form @submit.prevent="login">
+      <br />
       <label for="txtUsername">USERNAME</label>
+      <br />
       <input type="text" id="txtUsername" v-model="username" required />
       <br />
+      <br />
       <label for="txtPassword">PASSWORD</label>
+      <br />
       <input type="password" id="txtPassword" v-model="password" required />
+      <br />
       <input type="submit" value="LOGIN" />
     </form>
     <h5>
@@ -44,4 +49,69 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+@import "../assets/styles/variables.css";
+a {
+  color: var(--primary-color);
+  display: inline-block;
+  text-decoration: none;
+  font-weight: 400;
+}
+
+/* FORM TYPOGRAPHY*/
+
+input[type="button"],
+input[type="submit"],
+input[type="reset"] {
+  background-color: var(--primary-color);
+  border: none;
+  color: var(--secondary-color);
+  padding: 15px 80px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 20px;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+  margin: 30px 20px 10px 20px;
+  -webkit-transition: all 0.3s ease-in-out;
+  -moz-transition: all 0.3s ease-in-out;
+  -ms-transition: all 0.3s ease-in-out;
+  -o-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+}
+
+input[type="button"]:active,
+input[type="submit"]:active,
+input[type="reset"]:active {
+  -moz-transform: scale(0.95);
+  -webkit-transform: scale(0.95);
+  -o-transform: scale(0.95);
+  -ms-transform: scale(0.95);
+  transform: scale(0.95);
+}
+
+input[type="text"],
+input[type="password"],
+input[type="email"] {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 5px;
+  width: 10%;
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+}
+</style>
